62dc6b57a270791f009236cf00739f478d76e734
Gabriele Maggio <gabrielemaggio03@gmail.com>
2024-05-16 21:19:31 +0200

    Push

--- resources/views/livewire/create-announcement.blade.php
+++ resources/views/livewire/create-announcement.blade.php
@@ -1,21 +1,37 @@
 <div>
     <h1>Crea il tuo annuncio!</h1>
-    <form wire:submit.live="store">
+
+    @if (session()->has('message'))
+        <div class="flex flex-row justify-center my-2 alert alert-success">
+            {{ session('message')}}
+        </div>
+    @endif
+
+    <form wire:submit="store">
         @csrf
 
         <div class="mb-3">
             <label for="title">Titolo Annuncio</label>
-            <input wire:model="title" type="text" class="form-control">
+            <input wire:model.live="title" type="text" class="form-control @error('title') is-invalid @enderror" >
+            @error('title')
+                {{$message}}
+            @enderror
         </div>
         
         <div class="mb-3">
             <label for="body">Descrizione</label>
-            <textarea wire:model="body" type="text" class="form-control"></textarea>
+            <textarea wire:model.live="body" class="form-control @error('body') is-invalid @enderror"></textarea>
+            @error('body')
+                {{$message}}
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="price">Prezzo</label>
-            <input wire:model="price" type="number" class="form-control">
+            <input wire:model.live="price" type="number" step="0.01" class="form-control @error('price') is-invalid @enderror">
+            @error('price')
+                {{$message}}
+            @enderror
         </div>
 
         <button type="submit" class="btn btn-primary shadow px-4 py-2">Crea</button>
