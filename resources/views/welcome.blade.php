bbd37d1a793b5b73c73b1aa1a4f067bf132f5c97
Marco Balduini <marco.balduini93@gmail.com>
2024-05-21 18:49:54 +0200

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,14 +9,14 @@
                     @foreach ($announcements as $announcement)
                         <div class="col-12 col-md-4 my-4">
                             <div class="card shadow" style="width: 18rem;">
-                                <img src="https://picsum.photos/id/237/200/300" class="card-img-top p-3 rounded" alt="...">
+                                <img src="" class="card-img-top p-3 rounded" alt="...">
                                 <div class="card-body">
                                     <h5 class="card-title">{{ $announcement->title }}</h5>
                                     <p class="card-text">{{ $announcement->body }}</p>
                                     <p class="card-text">{{ $announcement->price }}</p>
                                     <a href="" class="btn btn-primary shadow">Visualizza</a>
                                     <a href="" class="my-2 border-top pt-2 border-dark card-link shadow btn 
-                                    btn-success">Categoria: {{ $announcement->category->name }}</a>
+                                    btn-success">Categoria: {{ $announcement->category }}</a>
                                     <p class="card-footer">Pubblicato il: {{ $announcement->created_at
                                     ->format('d/m/Y') }}</p>
                                 </div>
