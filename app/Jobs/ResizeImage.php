85dfd08c5c8d6344da3d5bb9be2627b0329bfa71
Faisal Mamun <tditaif98@gmail.com>
2024-05-29 19:51:12 +0200

    inizio use 6

---
+++ app/Jobs/ResizeImage.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ResizeImage implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $w;
+    private $h;
+    private $fileName;
+    private $path;
+
+    /**
+     * Create a new job instance.
+     * @return void 
+     */
+    public function __construct($filePath , $w , $h)
+    {
+        $this->path = dirname($filePath);
+        $this->fileName = basename($filePath);
+        $this->w = $w;
+        $this->h =$h;
+        
+    }
+
+    /**
+     * Execute the job.
+     * @return void
+     */
+    public function handle(): void
+    {
+        $w = $this->w;
+        $h = $this->h;
+        $scrPath = storage_path() . '/app/public' . $this->path . '/' . $this->fileName;
+        $destPath = storage_path() . '/app/public' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;
+    }
+}
